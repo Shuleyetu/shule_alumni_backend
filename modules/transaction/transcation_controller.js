239ffa3c21b4a1b3069e7e3daa9b6b900a0e68af
johnchuma <johnvchuma@gmail.com>
2023-07-19 11:25:09 +0300

    save changes

--- modules/transaction/transcation_controller.js
+++ modules/transaction/transcation_controller.js
@@ -1,7 +1,51 @@
 const { errorResponse, successResponse } = require("../../utils/responses")
-const {Transaction,Pledge,User} = require("../../models");
+const {Transaction,Pledge,User,Project} = require("../../models");
 
-
+const transactionRequest =  async (req,res)=>{
+    try {
+        const user_uuid = req.params.uuid;
+        const {pledge_uuid} = req.body;
+        const user = await User.findOne({
+            where:{
+                uuid:user_uuid
+            }
+        })
+        const pledge = await Pledge.findOne({
+            where:{
+                uuid:pledge_uuid
+            }
+        })
+        const project = await Project.findOne({
+            where:{
+                id:pledge.projectId
+            }
+        })
+        const response = await got.post("https://api.flutterwave.com/v3/payments", {
+            headers: {
+                Authorization: `Bearer ${process.env.FLW_SECRET_KEY}`
+            },
+            json: {
+                tx_ref: "hooli-tx-1920bbtytty",
+                amount: pledge.amount,
+                currency: "TZS",
+                redirect_url: `https://shulealumni.com/dashboard/projects/details/${project.uuid}`,
+                customer: {
+                    userId:user_uuid,
+                    pledgeId:pledge_uuid,
+                    email: user.email,
+                    phone_number: user.phone,
+                    name: user.name
+                },
+                customizations: {
+                    title: "Shule Alumni",
+                    logo: "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
+                }
+            }
+        }).json();
+    } catch (error) {
+        errorResponse(res,error)
+    }
+}
 const recordWebhookData = async (req, res) => {
     try {
         const secretHash = process.env.FLW_SECRET_HASH;
@@ -46,8 +90,6 @@ const recordWebhookData = async (req, res) => {
             customer_name: payload.customer.name,
             customer_phone: payload.customer.phone_number,
             customer_email: payload.customer.email,
-            card_first_6digits: payload.card.first_6digits,
-			card_last_4digits: payload.card.last_4digits,
             userId,
             pledgeId
         };

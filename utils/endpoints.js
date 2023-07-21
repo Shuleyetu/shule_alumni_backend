3722b11cb088f777db7d037c5be03614e8b233d7
johnchuma <johnvchuma@gmail.com>
2023-07-21 17:36:53 +0300

    save

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -1,4 +1,4 @@
-const production_endpoint = "https://shulealumnibackendat2023.onrender.com/"
+const production_endpoint = "http://89.116.179.120:5000/"
 // const production_endpoint = "http://localhost:5000/"
 
 module.exports = production_endpoint;

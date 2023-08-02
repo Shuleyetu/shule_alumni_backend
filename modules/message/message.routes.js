29fcb3ce039fdbf4459b34263882c9a4b9617782
johnchuma <johnvchuma@gmail.com>
2023-08-02 15:33:32 +0300

    save change

--- modules/message/message.routes.js
+++ modules/message/message.routes.js
@@ -3,7 +3,7 @@ const { getMessages, saveMessage, replyMessage } = require('./message.controller
 const router = Router()
 
 router.post("/",saveMessage)
-router.post("/reply",replyMessage)
+router.post("/reply/:uuid",replyMessage)
 router.get('/',getMessages)
 
 
